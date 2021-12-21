90d63b659b9799d9bbdfcf237fce8fc609ebd7c4
CatsMeow492 <calisurf243@hotmail.com>
2021-12-21 11:18:48 -0800

    started header

--- src/App.js
+++ src/App.js
@@ -1,30 +1,20 @@
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 import styled from 'styled-components';
 
 
 function App() {
   return (
     <div className="App">
-      
-      <AppHeader>
 
-        <img className=""
-          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
-          alt="" 
-          />
+      <Router>
+        <Header />
+      </Router>
 
-      </AppHeader>
     </div>
   );
 }
-
-const AppHeader = styled.div`
-  margin: 0;
-
-  img {
-    object-fit: contain;
-  }
-`;
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Header() {
+  return (
+ 
+      
+    <AppHeader>
+
+        <img className=""
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt="" 
+          />
+
+    </AppHeader>
+         
+  );
+}
+
+const AppHeader = styled.div`
+  margin: 0;
+  background-color: white;
+
+  img {
+    object-fit: contain;
+  }
+`;
+
+export default Header;
