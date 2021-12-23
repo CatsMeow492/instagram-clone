eee65d6ffb54bf2466e55e06343bba77e7aebc1a
CatsMeow492 <calisurf243@hotmail.com>
2021-12-23 10:51:18 -0800

    still adding styles

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,21 +1,36 @@
 import React from 'react'
 import styled from 'styled-components';
+import Avatar from '@mui/material/Avatar';
+import './Post.css';
 
 function Post() {
     return (
-        <div>
-            <h3>UserName</h3>
-            {/* header -> avatar + username */}
+        <PostBody>
+            <PostHeader>
+                <Avatar
+                className="post_avatar" 
+                alt='username'
+                src='https://lh3.googleusercontent.com/proxy/lHxVo9E7nJ28puugD1rJtLCCuVBrN1Lv9d1YJuWOW6-gH1PBgpkhSHaVH_uRL9kBX7Xt6pD52VAwlTgGr5zVP7yL_LDpBxsVMW1ET9cu0or8Kw3hhbDX2q270ZENs_ZkMEMuad6g' 
+                />
+                <h3>UserName</h3>
+            </PostHeader>
             <PostImg src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png' alt=''></PostImg>
-            {/* image */}
-            
-            {/* username + caption */}
 
             <PostText><strong>Username:</strong> Caption Lorum Ipsum Dolor</PostText>
 
-        </div>
+        </PostBody>
     )
 }
+
+const PostBody = styled.div`
+
+`;
+
+const PostHeader = styled.div`
+    display: flex;
+    align-items: center;
+    padding: 20px;
+`;
 
 const PostImg = styled.img`
     width: 100%;
@@ -23,6 +38,7 @@ const PostImg = styled.img`
 `;
 
 const PostText = styled.a`
+    font: normal;
 `;
 
 export default Post
