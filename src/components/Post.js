be9854f5b20e83bce627aed7890748e5176135c1
CatsMeow492 <calisurf243@hotmail.com>
2021-12-23 10:22:01 -0800

    adding styles to post

--- src/components/Post.js
+++ src/components/Post.js
@@ -6,18 +6,23 @@ function Post() {
         <div>
             <h3>UserName</h3>
             {/* header -> avatar + username */}
-            <PostImg></PostImg>
+            <PostImg src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png' alt=''></PostImg>
             {/* image */}
-
+            
             {/* username + caption */}
 
-            <h1>Hello World</h1>
+            <PostText><strong>Username:</strong> Caption Lorum Ipsum Dolor</PostText>
 
         </div>
     )
 }
 
 const PostImg = styled.img`
+    width: 100%;
+    object-fit: contain; 
+`;
+
+const PostText = styled.a`
 `;
 
 export default Post
