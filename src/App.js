ffcef47d443bf7f244191a19a435cb4a0fce13f1
CatsMeow492 <calisurf243@hotmail.com>
2022-01-05 18:49:20 -0800

    working on modals

--- src/App.js
+++ src/App.js
@@ -4,10 +4,33 @@ import './App.css';
 import Header from './components/Header';
 import Post from './components/Post';
 import { db } from './firebase';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
 
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
 
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+  const [modalStyle] = React.useState(getModalStyle);
   const [posts, setPosts] = useState([]); // post hook, fetch post data from state
   const [open, setOpen] = useState(false);
 
@@ -23,13 +46,16 @@ useEffect(() => {
   })
 }, []);
 
+
   return (
     <div className="App">
         <Modal
           open={open}
-          onClose={handleClose}
+          onClose={() => setOpen(false)}
         >
-          {body}
+          <div style={modalStyle} className={classes.paper}>
+            <h2>I am a modal</h2>
+          </div>
         </Modal>
       <title>instagram-clone</title>
       <Router>
