6203039444b2658e60f4c12ca45b2afe26e6397a
CatsMeow492 <calisurf243@hotmail.com>
2022-01-18 16:11:09 -0800

    added captions to ImageUpload dialogue

--- src/components/ImageUpload.js
+++ src/components/ImageUpload.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+function ImageUpload() {
+    const [caption, setCaption] = useState('')
+}
 
 function ImageUpload() {
     return (
         <div>
+            <h1>abc</h1>
             {/* I want to have ... */}
             {/* some kind of caption input */}
             {/* some kind of file picker */}
             {/* Post button */}
+            <input type='text' placeholder='Enter a caption..' onChange={event => setCaption(event.target.value)} value={caption} />
+            <input type='file' onChange={handleChanges} />
+
         </div>
     )
 }
